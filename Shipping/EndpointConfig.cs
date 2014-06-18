a2fc961cd974670e04d013500801dcd0b7216d32
Jimmy Bogard <jimmy.bogard@gmail.com>
2014-06-18 11:00:58 -0500

    UPgrading to 4.6

--- Shipping/EndpointConfig.cs
+++ Shipping/EndpointConfig.cs
@@ -1,13 +1,11 @@
 
-
-
 namespace Shipping
 {
     using NServiceBus;
 
 	/*
 		This class configures this endpoint as a Server. More information about how to configure the NServiceBus host
-		can be found here: http://nservicebus.com/GenericHost.aspx
+		can be found here: http://particular.net/articles/the-nservicebus-host
 	*/
 	public class EndpointConfig : IConfigureThisEndpoint, AsA_Server
     {
